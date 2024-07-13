ffc3ab4f56519641d068e6901bc0408c046756e8
Peter <richardsonp2@cardiff.ac.uk>
2024-07-13 10:24:27 +0100

    setting up models

--- app/models.py
+++ app/models.py
@@ -10,6 +10,10 @@ from sqlalchemy.orm import relationship
 from typing import List, Optional
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
 
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import DataRequired
+
 import sqlalchemy as sa
 import sqlalchemy.orm as so
 
@@ -17,24 +21,40 @@ import sqlalchemy.orm as so
 class Base(DeclarativeBase):
     pass
 
+"""Database for the hedgehogs including their name, age, and staff member. """
+
 class Hedgehogs(Base):
+    __tablename__ = 'hedgehogs'
     id: Mapped[int] = mapped_column(sa.Integer(), primary_key=True)
     name: Mapped[str] = mapped_column(sa.String(), nullable= False)
     age: Mapped[int] = mapped_column(sa.Integer(), nullable= True)
-    staff_member: Mapped[int] = ForeignKey
+    staff_member: Mapped[int] = ForeignKey('staff.id')
     # Need to add a relationship to the staff for many to one. Many hedgehogs but only one staff member.
+
+""" Databases for the staff members including the volunteers and permanent staff. """
 
 class Staff():
     __tablename__ = 'staff'
     id: Mapped[int] = mapped_column(sa.Integer(), primary_key= True)
     name: Mapped[str] = mapped_column(sa.String(), nullable= False)
+    hedgehog_under_care: Mapped[int] = ForeignKey('hedgehogs.id')
 
 class VolunteerStaff():
     
     __tablename__ = 'volunteer_staff'
+    id: Mapped[int] = mapped_column(sa.Integer(), primary_key= True)
+    hours: Mapped[int] = mapped_column(sa.Integer(), nullable= False)
 
 class PermanentStaff(Staff):
     __tablename__ = 'permanent_staff'
     id: Mapped[int] = mapped_column(sa.Integer(), primary_key= True)
     years_of_experience: Mapped[int] = mapped_column(sa.Integer(), nullable= False)
-    #Need to link to how many hedgehogs are in each staff members care
+
+"""Databases for all things related to the login and registration of the users"""
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember_me = BooleanField('remember_me')
+    submit = SubmitField('Sign In')
+
--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template
+from flask import render_template, flash, redirect, url_for
 from app import app
+from app.models import LoginForm
 
 @app.route('/')
 @app.route('/index')
@@ -9,3 +11,12 @@ def index():
 @app.route('/data_view')
 def data_view():
     return(render_template('data_view.html'))
+
+@app.route('/login', methods = ['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect(url_for('index'))
+    return render_template('login.html', title = "Sign in", form = form)
